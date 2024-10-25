1e0d91e38279a8d448a8adf9308d3f18db49f870
rana4678 <rana@shaficonsultancy.com>
2024-10-25 21:22:58 +0600

    Use img directly in Spinner modal

--- resources/js/components/SpinnerModal.jsx
+++ resources/js/components/SpinnerModal.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-import Spinner from "./Spinner";
+import spinner from "./images/spinner.gif";
 
 const SpinnerModal = () => {
     return (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
             <div className="bg-white p-6 rounded-md shadow-lg">
-                <Spinner />
+                <img
+                    src={spinner}
+                    alt="Loading..."
+                    style={{ width: "100px", display: "block", margin: "auto" }}
+                />
                 <p className="mt-2 text-center">Loading...</p>
             </div>
         </div>
